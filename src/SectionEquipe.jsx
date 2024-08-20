af365327562015516682c8af26997f201871af98
Adryelle Araujo <191036532940@academico.ufs.br>
2024-08-20 14:52:17 -0300

    atualização de emails e funções

--- src/SectionEquipe.jsx
+++ src/SectionEquipe.jsx
@@ -9,27 +9,27 @@ export function SectionEquipe() {
                 <h2 id="equipe">Equipe Desenvolvimento</h2>
                 <div className="container-main-card ">
                     <CardEquipe nome={"Adryelle Araujo"} funcao={"Desenvolvedor Front-end"} email={"adryelle.comp13@gmail.com"} classeTexto={"black-text"} />
-                    <CardEquipe nome={"Arthur Franklin"} funcao={"Desenvolvedor Back-end"} email={"adryelle.comp13@gmail.com"} classeTexto={"black-text"}/>
+                    <CardEquipe nome={"Arthur Franklin"} funcao={"Desenvolvedor Back-end"} email={"arthuroliveira.s@outlook.com.br"} classeTexto={"black-text"}/>
                     <CardEquipe nome={"Breno Gabriel"} funcao={"Desenvolvedor Front-end"} email={"adryelle.comp13@gmail.com"} classeTexto={"black-text"}/>
-                    <CardEquipe nome={"George Lucas"} funcao={"Desenvolvedor Back-end"} email={"adryelle.comp13@gmail.com"} classeTexto={"black-text"}/>
-                    <CardEquipe nome={"Gustavo Lucas"} funcao={"Desenvolvedor Front-end"} email={"adryelle.comp13@gmail.com"} classeTexto={"black-text"}/>
-                    <CardEquipe nome={"Luan Kyrtinem"} funcao={"Desenvolvedor Back-end"} email={"adryelle.comp13@gmail.com"} classeTexto={"black-text"}/>
-                    <CardEquipe nome={"Paulina Kayse"} funcao={"Desenvolvedor Back-end"} email={"adryelle.comp13@gmail.com"} classeTexto={"black-text"}/>
+                    <CardEquipe nome={"George Lucas"} funcao={"Desenvolvedor Back-end"} email={"georgelucas347@hotmail.com"} classeTexto={"black-text"}/>
+                    <CardEquipe nome={"Gustavo Lucas"} funcao={"Desenvolvedor Front-end"} email={"gustavolucas864@gmail.com"} classeTexto={"black-text"}/>
+                    <CardEquipe nome={"Luan Kyrtinem"} funcao={"Desenvolvedor Back-end"} email={"Kyrtinem@gmail.com"} classeTexto={"black-text"}/>
+                    <CardEquipe nome={"Paulina Kayse"} funcao={"Desenvolvedor Back-end"} email={"kaspaulina@hotmail.com"} classeTexto={"black-text"}/>
                 </div>
             </div>
             <div className='container-main-equipe verde'>
                 <h2>Especialistas de Domínio</h2>
                 <div className="container-main-card">
-                    <CardEquipe nome={"Letícia Vitória"} funcao={"Desenvolvedor Front-end"} email={"adryelle.comp13@gmail.com"} classeTexto={"verde-text"} classeTitulo={"verde-text"}/>
-                    <CardEquipe nome={"Suziany Caduda"} funcao={"Desenvolvedor Back-end"} email={"adryelle.comp13@gmail.com"} classeTexto={"verde-text"} classeTitulo={"verde-text"}/>
-                    <CardEquipe nome={"Walderi"} funcao={"Desenvolvedor Front-end"} email={"adryelle.comp13@gmail.com"} classeTexto={"verde-text"} classeTitulo={"verde-text"}/>
+                    <CardEquipe nome={"Letícia Vitória"} funcao={"Product Owner"} email={"adryelle.comp13@gmail.com"} classeTexto={"verde-text"} classeTitulo={"verde-text"}/>
+                    <CardEquipe nome={"Suziany Caduda"} funcao={"Product Owner"} email={"adryelle.comp13@gmail.com"} classeTexto={"verde-text"} classeTitulo={"verde-text"}/>
+                    <CardEquipe nome={"Walderi"} funcao={"Product Owner"} email={"adryelle.comp13@gmail.com"} classeTexto={"verde-text"} classeTitulo={"verde-text"}/>
                 </div>
                 <h2>Consultoria Técnica</h2>
                 <div className="container-main-card">
-                    <CardEquipe nome={"Debora"} funcao={"Desenvolvedor Front-end"} email={"adryelle.comp13@gmail.com"} classeTexto={"verde-text"} classeTitulo={"verde-text"}/>
-                    <CardEquipe nome={"Edilayne"} funcao={"Desenvolvedor Back-end"} email={"adryelle.comp13@gmail.com"} classeTexto={"verde-text"} classeTitulo={"verde-text"}/>
-                    <CardEquipe nome={"Edward Moreno"} funcao={"Desenvolvedor Back-end"} email={"adryelle.comp13@gmail.com"} classeTexto={"verde-text"} classeTitulo={"verde-text"}/>
-                    <CardEquipe nome={"Ricardo"} funcao={"Desenvolvedor Front-end"} email={"adryelle.comp13@gmail.com"} classeTexto={"verde-text"} classeTitulo={"verde-text"}/>
+                    <CardEquipe nome={"Debora"} funcao={"Orientadora"} email={"deboramcn@dcomp.ufs.br"} classeTexto={"verde-text"} classeTitulo={"verde-text"}/>
+                    <CardEquipe nome={"Edilayne"} funcao={"Orientadora"} email={"edilayne@dcomp.ufs.br"} classeTexto={"verde-text"} classeTitulo={"verde-text"}/>
+                    <CardEquipe nome={"Edward Moreno"} funcao={"Orientador"} email={"edward@dcomp.ufs.br"} classeTexto={"verde-text"} classeTitulo={"verde-text"}/>
+                    <CardEquipe nome={"Ricardo"} funcao={"Orientador"} email={"ricardo@dcomp.ufs.br"} classeTexto={"verde-text"} classeTitulo={"verde-text"}/>
                 </div>
             </div>
         </>
