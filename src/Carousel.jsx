1afb4178343778c610835428737fe7fbc9994389
Adryelle Araujo <191036532940@academico.ufs.br>
2024-09-09 16:15:38 -0300

    ajuste carrossel

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -3,10 +3,12 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Navigation, Pagination } from "swiper/modules";
-import celular from './assets/cellLg.png';
-import computador from './assets/computerLg.png';
-import todos from './assets/todosLg.png';
-import testefer from "./assets/fer.png"
+// import celular from './assets/cellLg.png';
+// import computador from './assets/computerLg.png';
+// import todos from './assets/todosLg.png';
+import celular from "./assets/fer.png";
+import todos from "./assets/todosDisp.png";
+import grafico from "./assets/grafico.png";
 import "./Carousel.css";
 
 export function Carousel() {
@@ -14,15 +16,29 @@ export function Carousel() {
     // Defina os conteúdos que você deseja renderizar
     const slides = [
         <div className="teste-carrossel" key="slide-1">
-            <img src={testefer} alt="Imagem do Slide 1" />
+            <img src={todos} alt="Imagem do Slide 1" />
             <div className="carrossel-text">
-                <h2>Débora é uma maluca chata</h2>
+                <h2>Disponível em todos os dispositivos</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eaque repellendus molestiae deserunt amet
                     officia impedit earum, debitis, illum assumenda commodi! Culpa dicta iste saepe est natus suscipit amet totam!</p>
             </div>
         </div>,
-        <div className="slide-content" key="slide-2">Conteúdo do Slide 2</div>,
-        <div className="slide-content" key="slide-3">Conteúdo do Slide 3</div>
+        <div className="teste-carrossel" key="slide-2">
+            <img src={grafico} alt="Imagem do Slide 1" />
+            <div className="carrossel-text">
+                <h2>Gráficos de evolução do paciente</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eaque repellendus molestiae deserunt amet
+                    officia impedit earum, debitis, illum assumenda commodi! Culpa dicta iste saepe est natus suscipit amet totam!</p>
+            </div>
+        </div>,
+        <div className="teste-carrossel" key="slide-3">
+            <img src={celular} alt="Imagem do Slide 1" />
+            <div className="carrossel-text">
+                <h2>Sessões de exercícios remotas</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eaque repellendus molestiae deserunt amet
+                    officia impedit earum, debitis, illum assumenda commodi! Culpa dicta iste saepe est natus suscipit amet totam!</p>
+            </div>
+        </div>
     ];
 
     return (
@@ -47,7 +63,7 @@ export function Carousel() {
                     {slides.map(slide => (
                     <SwiperSlide className="imagem">
                         <div className="teste-carrossel">
-                            <img src={testefer} alt={slides} />
+                            <img src={celular} alt={slides} />
                             <div>
                                 <h2>DEbora é uma maluca chata</h2>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eaque repellendus molestiae deserunt amet
@@ -55,9 +71,9 @@ export function Carousel() {
                             </div>
                         </div>
                         <div className="teste-carrossel">
-                            <img src={testefer} alt={slides} />
-                            <img src={testefer} alt={slides} />
-                            <img src={testefer} alt={slides} />
+                            <img src={celular} alt={slides} />
+                            <img src={celular} alt={slides} />
+                            <img src={celular} alt={slides} />
                             <div>
                                 <h2>DEbora é uma maluca chata</h2>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eaque repellendus molestiae deserunt amet
